aa45ae3de7b5d2668ff26a68437622a492dfc987
bahadirseyfi <bahadirsoftware@gmail.com>
2021-01-01 02:16:45 +0300

    Repeat-3

--- day-12-13-14.playground/Contents.swift
+++ day-12-13-14.playground/Contents.swift
@@ -107,3 +107,139 @@ func getHaterStatus(weather: WeatherType) -> String? {
 }
 
 getHaterStatus(weather: WeatherType.wind(speed: 5))
+
+
+struct Person {
+    var clothes: String
+    var shoes: String
+}
+
+let taylor = Person(clothes: "T-shirts", shoes: "sneakers")
+let other = Person(clothes: "short skirts", shoes: "high heels")
+
+print(taylor.clothes)
+print(other.shoes)
+
+
+struct Person {
+    var clothes: String
+    var shoes: String
+}
+
+let taylor = Person(clothes: "T-shirts", shoes: "sneakers")
+let other = Person(clothes: "short skirts", shoes: "high heels")
+
+var taylorCopy = taylor
+taylorCopy.shoes = "flip flops"
+
+print(taylor)
+print(taylorCopy)
+
+
+struct Person {
+    var clothes: String
+    var shoes: String
+
+    func describe() {
+        print("I like wearing \(clothes) with \(shoes)")
+    }
+}
+
+class Person {
+    var clothes: String
+    var shoes: String
+}
+
+class Singer {
+    var name: String
+    var age: Int
+
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+
+    func sing() {
+        print("La la la la")
+    }
+}
+
+var taylor = Singer(name: "Taylor", age: 25)
+taylor.name
+taylor.age
+taylor.sing()
+
+class CountrySinger: Singer {
+
+}
+
+class CountrySinger: Singer {
+    override func sing() {
+        print("Trucks, guitars, and liquor")
+    }
+}
+
+
+var taylor = CountrySinger(name: "Taylor", age: 25)
+taylor.sing()
+
+class HeavyMetalSinger: Singer {
+    var noiseLevel: Int
+
+    init(name: String, age: Int, noiseLevel: Int) {
+        self.noiseLevel = noiseLevel
+        super.init(name: name, age: age)
+    }
+
+    override func sing() {
+        print("Grrrrr rargh rargh rarrrrgh!")
+    }
+}
+
+struct Person {
+    var clothes: String
+    var shoes: String
+
+    func describe() {
+        print("I like wearing \(clothes) with \(shoes)")
+    }
+}
+
+let taylor = Person(clothes: "T-shirts", shoes: "sneakers")
+let other = Person(clothes: "short skirts", shoes: "high heels")
+taylor.describe()
+other.describe()
+
+
+struct Person {
+    var clothes: String {
+        willSet {
+            updateUI(msg: "I'm changing from \(clothes) to \(newValue)")
+        }
+
+        didSet {
+            updateUI(msg: "I just changed from \(oldValue) to \(clothes)")
+        }
+    }
+}
+
+func updateUI(msg: String) {
+    print(msg)
+}
+
+var taylor = Person(clothes: "T-shirts")
+taylor.clothes = "short skirts"
+
+
+struct Person {
+    var age: Int
+
+    var ageInDogYears: Int {
+        get {
+            return age * 7
+        }
+    }
+}
+
+var fan = Person(age: 25)
+print(fan.ageInDogYears)
