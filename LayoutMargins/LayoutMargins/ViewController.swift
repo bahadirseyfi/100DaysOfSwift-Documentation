d5f67ab16061ed8b8c0b17aa2d5bf98bb6811e42
bahadirseyfi <bahadirsoftware@gmail.com>
2021-03-17 12:48:56 +0300

    // Giving layout guides default height
    // Vertical Positions
    // Spacer equal widths
    // Trailing Guide

--- LayoutMargins/LayoutMargins/ViewController.swift
+++ LayoutMargins/LayoutMargins/ViewController.swift
@@ -34,11 +34,35 @@ class ViewController: UIViewController {
         let margins = view.layoutMarginsGuide
         
         // Leading Guide
-        margins.trailingAnchor.constraint(equalTo: trailingGuide.leadingAnchor).isActive = true
+        margins.leadingAnchor.constraint(equalTo: leadingGuide.leadingAnchor).isActive = true
         leadingGuide.trailingAnchor.constraint(equalTo: okButton.leadingAnchor).isActive = true
         
         // Middle Guide
-        okButton.trailingAnchor.constraint(equalTo: middleGuide.leadingAnchor)
+        okButton.trailingAnchor.constraint(equalTo: middleGuide.leadingAnchor).isActive = true
+        middleGuide.trailingAnchor.constraint(equalTo: cancelButton.leadingAnchor).isActive = true
+        
+        // Trailing Guide
+        cancelButton.trailingAnchor.constraint(equalTo: trailingGuide.leadingAnchor).isActive = true
+        trailingGuide.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
+        
+        // Button Equal Widths
+        okButton.widthAnchor.constraint(equalTo: cancelButton.widthAnchor).isActive = true
+        
+        // Spacer equal widths
+        leadingGuide.widthAnchor.constraint(equalTo: middleGuide.widthAnchor).isActive = true
+        leadingGuide.widthAnchor.constraint(equalTo: trailingGuide.widthAnchor).isActive = true
+        
+        // Vertical Positions
+        leadingGuide.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        middleGuide.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        trailingGuide.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        okButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        cancelButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        // Giving layout guides default height
+        leadingGuide.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        middleGuide.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        trailingGuide.heightAnchor.constraint(equalToConstant: 1).isActive = true
     }
     
     func makeButton(title: String, color: UIColor) -> UIButton {
