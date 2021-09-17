dc3f1bfd3f029aae104b7f51fe1b9275b2a09417
bahadirseyfi <bahadirsoftware@gmail.com>
2021-09-17 20:35:17 +0300

    Convenience & Required Init

---
+++ ConvenienceInit.playground/Contents.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+protocol CarType {
+    init(make: String, model: String)
+}
+
+final class Car: CarType {
+    var make: String
+    var model: String
+    var color: String
+    
+    init(make: String, model: String, color: String) {
+        self.make = make
+        self.model = model
+        self.color = color
+    }
+    
+    convenience init(make: String, model: String) {
+        self.init(make: make, model: model, color: "White")
+    }
+    /*
+    class func makeCar(make: String, model: String) -> Self {
+        let car = self.init(make: make, model: model)
+        // setup engine
+        // setup fuel lines
+        // setup tires
+        return car
+    }
+    */
+}
+
+let car = Car(make: "deneme", model: "dene")
+print(car.color)
+
+
+class Tesla: Car {
+    var range: Double = 300
+    
+    override init(make: String, model: String, color: String) {
+        self.range = 300
+        super.init(make: make, model: model, color: color)
+    }
+}
+
+let tesla = Tesla(make: "", model: "")
+print(tesla.range)
+
+
